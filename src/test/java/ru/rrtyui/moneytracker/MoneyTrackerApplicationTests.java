b821b8240db133747781bbfa10415ceed61734e1
Андрей <rrtyuisess@gmail.com>
2026-03-06 14:15:01 +0300

    Initial commit

---
+++ src/test/java/ru/rrtyui/moneytracker/MoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rrtyui.moneytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
