08d822384dd4390bc4db95ad0ac04a373d96e4b9
Андрей <rrtyuisess@gmail.com>
2026-03-07 18:44:06 +0300

    FRONT-2: обновить компоненты формы с выбором категории

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4,3 +4,18 @@ export interface CategoryResponse {
     parentId: number | null;
     hasChildren: boolean;
 }
+export interface TransactionRequest {
+    amount: number;
+    date: string;
+    description: string;
+    categoryId: number;
+}
+
+export interface TransactionResponse {
+    id: number;
+    amount: number;
+    date: string;
+    description: string;
+    categoryId: number;
+    categoryName: string;
+}
