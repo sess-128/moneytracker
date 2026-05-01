f0fcd17a8a4ab937d38e4ce96db1e4497ff8a799
Nikon <102325439+MrNikaMilon@users.noreply.github.com>
2026-05-01 16:10:57 +0300

    Fixed security error

--- backend/src/main/java/ru/rrtyui/moneytracker/config/SecurityConfig.java
+++ backend/src/main/java/ru/rrtyui/moneytracker/config/SecurityConfig.java
@@ -1,63 +1,63 @@
-package ru.rrtyui.moneytracker.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ru.rrtyui.moneytracker.filter.JwtAuthentificationFilter;
-import ru.rrtyui.moneytracker.service.MTUserService;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final JwtAuthentificationFilter jwtAuthentificationFilter;
-    private final MTUserService userService;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthentificationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userService.userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-}
+//package ru.rrtyui.moneytracker.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import ru.rrtyui.moneytracker.filter.JwtAuthentificationFilter;
+//import ru.rrtyui.moneytracker.service.MTUserService;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final JwtAuthentificationFilter jwtAuthentificationFilter;
+//    private final MTUserService userService;
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(request -> request
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
+//                        .anyRequest().authenticated())
+//                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authenticationProvider(authenticationProvider())
+//                .addFilterBefore(jwtAuthentificationFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userService.userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+//}
--- backend/src/main/java/ru/rrtyui/moneytracker/service/MTUserService.java
+++ backend/src/main/java/ru/rrtyui/moneytracker/service/MTUserService.java
@@ -1,12 +1,11 @@
 package ru.rrtyui.moneytracker.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.rrtyui.moneytracker.model.MTUser;
 import ru.rrtyui.moneytracker.repository.MTUserRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +18,9 @@ public class MTUserService {
     }
 
     public MTUser create(MTUser user) {
-        if (userRepository.existsByName(user.getUsername())) {
-            throw new RuntimeException("Пользователь с таким именем уже существует");
-        }
+//        if (userRepository.existsByName(user.getUsername())) {
+//            throw new RuntimeException("Пользователь с таким именем уже существует");
+//        }
 
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("Пользователь с такой почтой уже существует");
@@ -32,15 +31,19 @@ public class MTUserService {
 
     public MTUser getByUsername(String username) {
         return userRepository.findByName(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
     }
 
-    public UserDetailsService userDetailsService() {
-        return this::getByUsername;
+    public Optional<MTUser> getUserByUsername(String username) {
+        return userRepository.findByName(username);
     }
-
-    public MTUser getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return getByUsername(username);
-    }
+//
+//    public UserDetailsService userDetailsService() {
+//        return this::getByUsername;
+//    }
+//
+//    public MTUser getCurrentUser() {
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        return getByUsername(username);
+//    }
 }
